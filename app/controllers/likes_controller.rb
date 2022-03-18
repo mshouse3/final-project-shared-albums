ff777a097c08a2d72e3451965232146901a22ce1
mshouse3 <mshouse@chicagobooth.edu>
2022-03-18 15:43:24 +0000

    maybe done

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,6 +1,7 @@
 class LikesController < ApplicationController
   def index
-    matching_likes = Like.all
+    @the_photo = Photo.where({ :id => params.fetch("photo_id") }).first
+    matching_likes = @the_photo.likes
 
     @list_of_likes = matching_likes.order({ :created_at => :desc })
 
@@ -19,14 +20,14 @@ class LikesController < ApplicationController
 
   def create
     the_like = Like.new
-    the_like.user_id = params.fetch("query_user_id")
+    the_like.user_id = @current_user.id
     the_like.photo_id = params.fetch("query_photo_id")
 
     if the_like.valid?
       the_like.save
-      redirect_to("/likes", { :notice => "Like created successfully." })
+      redirect_to("/users/#{the_like.photo.album.owner.username}/albums/#{the_like.photo.album.title}/photos/#{the_like.photo.id}/likes", { :notice => "Comment created successfully." })
     else
-      redirect_to("/likes", { :alert => like.errors.full_messages.to_sentence })
+      redirect_to("/users/#{the_like.photo.album.owner.username}/albums/#{the_like.photo.album.title}/photos/#{the_like.photo.id}/likes", { :alert => like.errors.full_messages.to_sentence })
     end
   end
 
@@ -39,7 +40,7 @@ class LikesController < ApplicationController
 
     if the_like.valid?
       the_like.save
-      redirect_to("/likes/#{the_like.id}", { :notice => "Like updated successfully."} )
+      redirect_to("/likes/#{the_like.id}", { :notice => "You liked this photo."} )
     else
       redirect_to("/likes/#{the_like.id}", { :alert => like.errors.full_messages.to_sentence })
     end
@@ -48,9 +49,10 @@ class LikesController < ApplicationController
   def destroy
     the_id = params.fetch("path_id")
     the_like = Like.where({ :id => the_id }).at(0)
+    the_photo = the_like.photo
 
     the_like.destroy
 
-    redirect_to("/likes", { :notice => "Like deleted successfully."} )
+    redirect_to("/users/#{the_photo.album.owner.username}/albums/#{the_photo.album.title}/photos/#{the_photo.id}/likes", { :notice => "Photo unliked."} )
   end
 end
